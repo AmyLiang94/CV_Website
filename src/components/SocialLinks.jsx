a10211f814c18d68cefbd5369fb06e37d8b417f8
YiTing Liang <amyliang@yitingdeMacBook-Air.local>
2024-11-14 22:43:41 +0000

    CV update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30}/>
                 </>
             ),
-            href:'/resume.pdf',
+            href:'/Amy Liang CV.pdf',
             style: 'rounded-tr-md',
             download: true,
         },
