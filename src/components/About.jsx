8406de1e63ef3453e4ae0d0823728fad6a63165f
YiTing Liang <amyliang@yitingdeMacBook-Air.local>
2024-11-15 16:20:04 +0000

    about height

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full justify-center h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full justify-center min-h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
