52ef5bf667498e9fb0fcb240c36fcc9fd15de34c
YiTing Liang <amyliang@yitingdeMacBook-Air.local>
2025-01-08 07:20:19 +0000

    dependency update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
+import Contribute from "./components/Contribute";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-import Contribute from "./components/Contribute";
+
 
 function App() {
   return (
