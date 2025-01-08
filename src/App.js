daf18d753932cd4b10aff49cf344e658fc7a0ace
YiTing Liang <amyliang@yitingdeMacBook-Air.local>
2025-01-08 07:05:14 +0000

    import contribute part

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import Contribute from "./components/Contribute";
 
 function App() {
   return (
