478b4a5df79f134625e96441f5d722a523305aa4
YiTing Liang <amyliang@yitingdeMacBook-Air.local>
2025-03-23 15:33:35 +0000

    tech skills description area

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
     {
       title: 'Technical Skills',
       description: [
-        'Proficiency: Currently working on web development projects for Northumbria University, collaborating with marketing teams to enhance user engagement. Also led the management and enhancement of the CMS backend system.',
-        'API Integration: Developed and deployed several full-stack web applications, gaining hands-on experience with projects like a money management website and a weather forecast app, showcasing strong skills in web development and RESTful API integration.',
-        'Database: Demonstrated strong database management skills through academic projects, including an Academic Staff Management System (Java, JUnit) that earned a grade of 85.5/100. Designed, implemented, and unit-tested a system for efficiently managing university staff data, showcasing practical knowledge of relational databases and SQL concepts.',
+        <span> <strong>Proficiency:</strong> Currently working on web development projects for Northumbria University, collaborating with marketing teams to enhance user engagement. Also led the management and enhancement of the CMS backend system.</span>,
+        <span> <strong>API Integration:</strong>Developed and deployed several full-stack web applications, gaining hands-on experience with projects like a money management website and a weather forecast app, showcasing strong skills in web development and RESTful API integration.</span>,
+        <span> <strong>Database:</strong> Demonstrated strong database management skills through academic projects, including an Academic Staff Management System (Java, JUnit) that earned a grade of 85.5/100. Designed, implemented, and unit-tested a system for efficiently managing university staff data, showcasing practical knowledge of relational databases and SQL concepts.</span>,
       ],
       bgColor: 'bg-blue-500',
       icon: <FaCode size={30} className="text-white" />,
